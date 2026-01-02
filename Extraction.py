9d306bdcefa7411aeaa00bbe79d47d737418d4b7
123bruke <bababruke@gmail.com>
2026-01-02 21:43:54 +0300

    Update Extraction.py

--- Extraction.py
+++ Extraction.py
@@ -1,6 +1,8 @@
 import pytesseract
 import cv2
 import re
+import speech_recognition as sr
+import fitz
 
 def extract_equation_from_image(image_path):
     img = cv2.imread(image_path)
@@ -8,4 +10,21 @@ def extract_equation_from_image(image_path):
     text = pytesseract.image_to_string(gray)
     lines = re.findall(r'[^\n]+', text)
     return ' '.join(lines).strip()
-  
+
+def extract_equation_from_pdf(pdf_path):
+    doc = fitz.open(pdf_path)
+    text_list = [page.get_text() for page in doc]
+    text = ' '.join(text_list)
+    lines = re.findall(r'[^\n]+', text)
+    return ' '.join(lines).strip()
+
+def extract_equation_from_audio(audio_path):
+    r = sr.Recognizer()
+    with sr.AudioFile(audio_path) as source:
+        audio = r.record(source)
+    try:
+        text = r.recognize_google(audio, language="th-TH")
+    except:
+        text = ""
+    lines = re.findall(r'[^\n]+', text)
+    return ' '.join(lines).strip()
